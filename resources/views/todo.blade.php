a431255d05cf87766cae5810f1d468cdab686006
hanschrs <hans_christian@ymail.com>
2017-03-13 11:24:21 +0700

    initial commit

---
+++ resources/views/todo.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ToDo List</title>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<h1>ToDo List</h1>
+				<ul>
+					<li>Taki</li>
+					<li>Mitsuha</li>
+					<li>Yotsuha</li>
+				</ul>
+				<form>
+					<div class="form-group">
+						<input type="text" name="todo_item" placeholder="Item baru...">
+					</div>
+					<div class="col-lg-1">
+						<button type="submit" class="btn btn-success">Add</button>
+					</div>
+					<div class="col-lg-8">
+						
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" type="text/javascript"></script>
+
+</body>
+</html>
